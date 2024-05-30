29fb6a9e991f61d7e79c5a50d919a0c50db06867
bakeable <robin@bakeable.nl>
2024-05-30 13:17:25 +0200

    Import AkeneoAttribute

--- akeneo_connector/__init__.py
+++ akeneo_connector/__init__.py
@@ -1,3 +1,4 @@
 from .akeneo_connector import AkeneoConnector
 from .akeneo_paginator import AkeneoPaginator
 from .akeneo_product import AkeneoProduct
+from .akeneo_attribute import AkeneoAttribute
