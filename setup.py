ebbb03898a9f654b5b43b5772faf5bf2bf5b9729
GitHub Action <action@github.com>
2024-05-30 10:47:57 +0000

    Bump minor version [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="akeneo_connector",
-    version="0.1.12",
+    version="0.1.13",
     packages=find_packages(),
     install_requires=[
         'requests >= 2.31.0',
